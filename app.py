ecd04fe9107423f1f5a6c1a4eb63ed9b0b13c91d
Alice Birkett <alice.birkett@outlook.com>
2023-08-14 12:14:51 +0100

    Completed practise routes

--- app.py
+++ app.py
@@ -16,6 +16,31 @@ app = Flask(__name__)
 def get_emoji():
     return ":)"
 
+@app.route('/hello', methods=['GET'])
+def hello():
+    name = request.args['name'] # The value is 'David'
+
+    # Send back a friendly greeting with the name
+    return f"Hello {name}!"
+
+@app.route('/goodbye', methods=['POST'])
+def goodbye():
+    name = request.form['name'] # The value is 'Alice'
+
+    # Send back a fond farewell with the name
+    return f"Goodbye {name}!"
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    name = request.form['name']
+    message = request.form['message']
+    return f"Thanks {name}, you sent this message: {message}"
+
+@app.route('/wave', methods=['GET'])
+def wave():
+    name = request.args['name']
+    return f"I am waving at {name}"
+
 # This imports some more example routes for you to see how they work
 # You can delete these lines if you don't need them.
 from example_routes import apply_example_routes
@@ -28,4 +53,3 @@ apply_example_routes(app)
 # if started in test mode.
 if __name__ == '__main__':
     app.run(debug=True, port=int(os.environ.get('PORT', 5000)))
-
